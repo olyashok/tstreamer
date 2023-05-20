2dc03de5de25b7aed948e9937cfe2d2978c13b65
olyashok <lyashok@gmail.com>
2023-05-20 17:40:05 -0400

    streamer

--- tstreamer/purge.sh
+++ tstreamer/purge.sh
@@ -19,6 +19,6 @@ st=2
 lt=14
 
 find "$cropdir" -type f -regex ".*_nobox_.*\.jpg" -mtime +$min -delete
-find "$cropdir" -type f -regex ".*_yolov[0-9]*[a-z]*-[0-9]*_.*\.jpg"  -mtime +$min -delete
+find "$cropdir" -type f -regex ".*_yolov[0-9]*[a-z]*_.*\.jpg"  -mtime +$min -delete
 find "$cropdir" -type f -regex ".*_box_.*\.jpg" -mtime +$st -delete
 find "$cropdir" -type f -regex ".*_unifi[a-z0-9]*_.*\.jpg"  -mtime +$lt -delete
